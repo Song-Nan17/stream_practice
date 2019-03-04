c5ba40cdd613219f487f2ad6c71a872bb801373a
Song <songnan17@qq.com>
2019-03-04 18:49:12 +0800

    Complete function divideToSmaller.

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -4,18 +4,23 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        int randomInt = this.random.nextInt(3);
+        int length = number / randomInt;
+        return Stream.iterate(number - randomInt, n -> n - randomInt).limit(length).collect(Collectors.toList());
     }
 }
