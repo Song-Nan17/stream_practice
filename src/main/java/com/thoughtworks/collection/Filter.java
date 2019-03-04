f3b98701717358590312198da3ffa3593572f68e
Song <songnan17@qq.com>
2019-03-04 18:25:29 +0800

    Complete function getCommonElements.

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -23,7 +23,7 @@ public class Filter {
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        return firstList.stream().filter(x -> secondList.contains(x)).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
